0b546d314a4f18578b2f1bc4da0ca9ab8325369d
Elena Vasyukova <evvasyukova29@yandex.ru>
2023-06-13 13:23:15 +0300

    First commit: New Test for Cat

---
+++ src/test/java/TestForCat.java
@@ -0,0 +1,35 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestForCat
+{
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getSoundTest()
+    {
+        var cat = new Cat(new Feline());
+        String expectedSound = "Мяу";
+        String actualSound = cat.getSound();
+        Assert.assertEquals(expectedSound, actualSound);
+    }
+
+    public void getFoodTest() throws Exception
+    {
+        var cat = new Cat(feline);
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expectedFood);
+        List<String> actualFood = cat.getFood();
+        Assert.assertEquals(expectedFood, actualFood);
+    }
+}
